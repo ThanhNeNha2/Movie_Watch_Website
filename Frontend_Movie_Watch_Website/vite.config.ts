d5a542d9126392bab525efa9a74e7515b106b239
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-02-23 00:27:41 +0700

    last code vercel 1

--- Frontend_Movie_Watch_Website/vite.config.ts
+++ Frontend_Movie_Watch_Website/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Quan trọng để tránh lỗi 404
+  base: "/", // Quan trọng để tránh lỗi 404
   build: {
     outDir: "dist",
   },
