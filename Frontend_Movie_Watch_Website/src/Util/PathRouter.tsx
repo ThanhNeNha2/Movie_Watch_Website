26121b90965f33d2334749e99ba529a909379b42
mtriet100505 <ta.coaching.v2@gmail.com>
2025-06-27 18:39:33 +0700

    get movie to api

--- Frontend_Movie_Watch_Website/src/Util/PathRouter.tsx
+++ Frontend_Movie_Watch_Website/src/Util/PathRouter.tsx
@@ -3,12 +3,11 @@ import Home from "../Page/Client/Home/Home";
 import DetailMovies from "../Page/Client/DetailMovies/DetailMovies";
 import Play from "../Page/Client/Play/Play";
 import ContentSearch from "../Page/Client/Search/ContentSearch";
-
 const PathRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/detailmovies" element={<DetailMovies />} />
+      <Route path="/detailmovies/:movieName" element={<DetailMovies />} />
       <Route path="/play" element={<Play />} />
       <Route path="/contentSearch" element={<ContentSearch />} />
     </Routes>
